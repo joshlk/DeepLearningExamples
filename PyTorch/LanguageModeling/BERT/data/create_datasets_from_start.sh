c60868670b20ca24344e09ee72d9cedc32c5453a
Josh Levy-Kramer <joshl@graphcore.ai>
2021-09-01 10:00:02 +0000

    Make data prep more general

--- PyTorch/LanguageModeling/BERT/data/create_datasets_from_start.sh
+++ PyTorch/LanguageModeling/BERT/data/create_datasets_from_start.sh
@@ -17,20 +17,20 @@ to_download=${1:-"wiki_only"}
 
 #Download
 if [ "$to_download" = "wiki_books" ] ; then
-    python3 /workspace/bert/data/bertPrep.py --action download --dataset bookscorpus
+    python3 bertPrep.py --action download --dataset bookscorpus
 fi
 
-python3 /workspace/bert/data/bertPrep.py --action download --dataset wikicorpus_en
-python3 /workspace/bert/data/bertPrep.py --action download --dataset google_pretrained_weights  # Includes vocab
-python3 /workspace/bert/data/bertPrep.py --action download --dataset squad
-python3 /workspace/bert/data/bertPrep.py --action download --dataset mrpc
-python3 /workspace/bert/data/bertPrep.py --action download --dataset sst-2
+python3 bertPrep.py --action download --dataset wikicorpus_en
+python3 bertPrep.py --action download --dataset google_pretrained_weights  # Includes vocab
+python3 bertPrep.py --action download --dataset squad
+python3 bertPrep.py --action download --dataset mrpc
+python3 bertPrep.py --action download --dataset sst-2
 
 # Properly format the text files
 if [ "$to_download" = "wiki_books" ] ; then
-    python3 /workspace/bert/data/bertPrep.py --action text_formatting --dataset bookscorpus
+    python3 bertPrep.py --action text_formatting --dataset bookscorpus
 fi
-python3 /workspace/bert/data/bertPrep.py --action text_formatting --dataset wikicorpus_en
+python3 bertPrep.py --action text_formatting --dataset wikicorpus_en
 
 if [ "$to_download" = "wiki_books" ] ; then
     DATASET="books_wiki_en_corpus"
@@ -40,12 +40,12 @@ else
 fi
 
 # Shard the text files
-python3 /workspace/bert/data/bertPrep.py --action sharding --dataset $DATASET
+python3 bertPrep.py --action sharding --dataset $DATASET
 
 # Create HDF5 files Phase 1
-python3 /workspace/bert/data/bertPrep.py --action create_hdf5_files --dataset $DATASET --max_seq_length 128 \
+python3 bertPrep.py --action create_hdf5_files --dataset $DATASET --max_seq_length 128 \
 --max_predictions_per_seq 20 --vocab_file $BERT_PREP_WORKING_DIR/download/google_pretrained_weights/uncased_L-24_H-1024_A-16/vocab.txt --do_lower_case 1
 
 # Create HDF5 files Phase 2
-python3 /workspace/bert/data/bertPrep.py --action create_hdf5_files --dataset $DATASET --max_seq_length 512 \
+#python3 /workspace/bert/data/bertPrep.py --action create_hdf5_files --dataset $DATASET --max_seq_length 512 \
 --max_predictions_per_seq 80 --vocab_file $BERT_PREP_WORKING_DIR/download/google_pretrained_weights/uncased_L-24_H-1024_A-16/vocab.txt --do_lower_case 1
