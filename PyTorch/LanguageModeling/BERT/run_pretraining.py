222e0e9932758e8c3472cacb0790a7896459fc7b
Josh Levy-Kramer <joshl@graphcore.ai>
2021-09-17 12:47:57 +0200

    Add act grad hooks

--- PyTorch/LanguageModeling/BERT/run_pretraining.py
+++ PyTorch/LanguageModeling/BERT/run_pretraining.py
@@ -608,10 +608,10 @@ def main():
                     prediction_scores, seq_relationship_score = model(input_ids=input_ids, token_type_ids=segment_ids, attention_mask=input_mask)
                     loss, mlm_loss, nsp_loss = criterion(prediction_scores, seq_relationship_score, masked_lm_labels, next_sentence_labels)
 
-                    if training_steps % (args.log_freq * args.gradient_accumulation_steps) == 0 and using_wandb:
-                        # Add wandb hooks to gradients of activations of classification heads
-                        prediction_scores.register_hook(lambda grad: print('GRAD1', grad)) #wandb.log({'activation_gradients/prediction_scores': grad.mean()}))
-                        seq_relationship_score.register_hook(lambda grad: print('GRAD1', grad)) #wandb.log({'activation_gradients/seq_relationship_score': grad}))
+                    #if training_steps % (args.log_freq * args.gradient_accumulation_steps) == 0 and using_wandb:
+                    #    # Add wandb hooks to gradients of activations of classification heads
+                    prediction_scores.register_hook(lambda grad: print('GRAD1', grad)) #wandb.log({'activation_gradients/prediction_scores': grad.mean()}))
+                    seq_relationship_score.register_hook(lambda grad: print('GRAD1', grad)) #wandb.log({'activation_gradients/seq_relationship_score': grad}))
 
                     if args.n_gpu > 1:
                         loss = loss.mean()  # mean() to average on multi-gpu.
